d573443faa23250ea17aa7ee551d3a28eae94451
Valentin <valengonzalez2015@hotmail.com>
2025-11-10 14:16:09 -0300

    consola utils
    agregue el mismo metodo pero para usarlo con double

--- src/Clases/ConsolaUtils.java
+++ src/Clases/ConsolaUtils.java
@@ -14,9 +14,27 @@ public class ConsolaUtils {
                 numero = scanner.nextInt();
                 esValido = true; // Si llegamos aca, es un número, salimos del bucle
             } catch (InputMismatchException e) {
-                System.out.println("Error: Debe ingresar un número. Intente de nuevo.");
+                System.out.println("Error: Debe ingresar un número. Intente de nuevo");
             } finally {
                 scanner.nextLine(); //  Limpia el buffer del scanner siempre.
+            }
+        }
+        return numero;
+    }
+
+    public static double leerDouble(Scanner scanner, String mensaje) {
+        double numero = 0.0;
+        boolean esValido = false;
+
+        while (!esValido) {
+            try {
+                System.out.println(mensaje);
+                numero = scanner.nextDouble();
+                esValido = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Error: Debe ingresar un número. Intente de nuevo");
+            } finally {
+                scanner.nextLine(); // Limpiar el buffer
             }
         }
         return numero;
