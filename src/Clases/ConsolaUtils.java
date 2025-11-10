28635651e75cf26ec23e1b09342a6a3dba75216e
Valentin <valengonzalez2015@hotmail.com>
2025-11-10 12:48:39 -0300

    Cree una clase para validar numeros enteros y double de entrada
    hice un metodo para poder reutilizarlo donde queramps

---
+++ src/Clases/ConsolaUtils.java
@@ -0,0 +1,23 @@
+package Clases;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class ConsolaUtils {
+    public static int leerEntero(Scanner scanner, String mensaje) {
+        int numero = 0;
+        boolean esValido = false;
+
+        while (!esValido) {
+            try {
+                System.out.println(mensaje);
+                numero = scanner.nextInt();
+                esValido = true; // Si llegamos aquí, es un número, salimos del bucle
+            } catch (InputMismatchException e) {
+                System.out.println("Error: Debe ingresar un número. Intente de nuevo.");
+            } finally {
+                scanner.nextLine(); // ¡IMPORTANTE! Limpia el buffer del scanner siempre.
+            }
+        }
+        return numero;
+    }
+
+}
