e1c5c31a13554bb3e1b07af01315cbc1082b734e
anibaldb <128552893+anibaldb@users.noreply.github.com>
2025-11-11 22:06:24 -0300

    main ordenado

--- src/Main.java
+++ src/Main.java
@@ -16,65 +16,55 @@ public class Main {
 
         Hotel miHotel;
         File archivo = new File("hotelDatos.json");
+
         if (archivo.exists()) {
             System.out.println("Cargando datos del hotel...");
             String contenido = JSONUtiles.downloadJSON("hotelDatos");
             JSONObject jsonHotelLeido = new JSONObject(contenido);
             miHotel = Hotel.fromJSON(jsonHotelLeido); }
-        else { System.out.println("Creando un nuevo hotel vacío...");
-                miHotel = new Hotel("Gran Hotel");
 
+        else {
+             System.out.println("Creando un nuevo hotel vacío...");
+             miHotel = new Hotel("Gran Hotel");
 
+             try {
+                 miHotel.agregarHabitacion(TipoHabitacion.SIMPLE, 10000);
+                 miHotel.agregarHabitacion(TipoHabitacion.SIMPLE, 10000);
+                 miHotel.agregarHabitacion(TipoHabitacion.DOBLE, 15000);
+                 miHotel.agregarHabitacion( TipoHabitacion.DOBLE, 15000);
+                 miHotel.agregarHabitacion(TipoHabitacion.DOBLE, 20000);
+                 miHotel.agregarHabitacion(TipoHabitacion.SIMPLE, 20000);
+                 miHotel.agregarHabitacion(TipoHabitacion.SUITE, 30000);
+                 miHotel.agregarHabitacion( TipoHabitacion.SUITE, 30000);
+                 miHotel.agregarHabitacion(TipoHabitacion.SIMPLE, 10000);
+                 miHotel.agregarHabitacion( TipoHabitacion.DOBLE, 15000);
 
-            try {
-                miHotel.agregarHabitacion(TipoHabitacion.SIMPLE, 10000);
-                miHotel.agregarHabitacion(TipoHabitacion.SIMPLE, 10000);
-                miHotel.agregarHabitacion(TipoHabitacion.DOBLE, 15000);
-                miHotel.agregarHabitacion( TipoHabitacion.DOBLE, 15000);
-                miHotel.agregarHabitacion(TipoHabitacion.DOBLE, 20000);
-                miHotel.agregarHabitacion(TipoHabitacion.SIMPLE, 20000);
-                miHotel.agregarHabitacion(TipoHabitacion.SUITE, 30000);
-                miHotel.agregarHabitacion( TipoHabitacion.SUITE, 30000);
-                miHotel.agregarHabitacion(TipoHabitacion.SIMPLE, 10000);
-                miHotel.agregarHabitacion( TipoHabitacion.DOBLE, 15000);
-
-            }catch (ExceptionHabitacionDuplicada e){
-                e.getMessage();
-            }
+             }catch (ExceptionHabitacionDuplicada e){
+                 e.getMessage();
+             }
         }
-
-
-
-       // miHotel.guardarEnJSON();
 
         SistemaUsuarios sistemaUsuarios;
         File archivoUsuarios = new File("usuarios.json");
 
         if (archivoUsuarios.exists()) {
-            System.out.println("Cargando usuarios...");
-            String contenido = JSONUtiles.downloadJSON("usuarios");
-            JSONObject jsonUsuariosLeido = new JSONObject(contenido);
-            sistemaUsuarios = SistemaUsuarios.fromJSON(jsonUsuariosLeido);
+             System.out.println("Cargando usuarios...");
+             String contenido = JSONUtiles.downloadJSON("usuarios");
+             JSONObject jsonUsuariosLeido = new JSONObject(contenido);
+             sistemaUsuarios = SistemaUsuarios.fromJSON(jsonUsuariosLeido);
+
         } else {
-            System.out.println("Creando nuevo sistema de usuarios...");
-            sistemaUsuarios = new SistemaUsuarios();
+             System.out.println("Creando nuevo sistema de usuarios...");
+             sistemaUsuarios = new SistemaUsuarios();
 
-            // Si querés, podés registrar algunos usuarios por defecto aquí
-            try {
-                sistemaUsuarios.registrarCliente("Anibal", 29763522, "Argentina", "Martinez 3340", "cli", "cli");
-                sistemaUsuarios.registrarRecepcionista("Mabel", 29040871, "Argentina", "Martinez 3340", "recep", "recep");
-                sistemaUsuarios.registrarAdministrador("Carlos", 29880648, "Cuba", "Martinez 3340", "admin", "admin");
-            } catch (ExceptionUsuarioDuplicado e) {
-                System.out.println(e.getMessage());
-            }
+             try {
+                 sistemaUsuarios.registrarCliente("Anibal", 29763522, "Argentina", "Martinez 3340", "cli", "cli");
+                 sistemaUsuarios.registrarRecepcionista("Mabel", 29040871, "Argentina", "Martinez 3340", "recep", "recep");
+                 sistemaUsuarios.registrarAdministrador("Carlos", 29880648, "Cuba", "Martinez 3340", "admin", "admin");
+             } catch (ExceptionUsuarioDuplicado e) {
+                 System.out.println(e.getMessage());
+             }
         }
-
-
-      //  sistemaUsuarios.guardarEnJSON();
-
-
-
-
 
         miHotel.setSistemaUsuarios(sistemaUsuarios);
         Scanner teclado=new Scanner(System.in);
@@ -87,10 +77,10 @@ public class Main {
             System.out.println("0- Salir\n");
 
             opcion = ConsolaUtils.leerEntero(teclado, "Ingrese opcion: ");
+            teclado.nextLine();
 
             switch (opcion) {
                 case 1 -> {
-
                     System.out.println("Ingrese eMail: ");
 
                     String mail = teclado.nextLine();
@@ -107,16 +97,13 @@ public class Main {
                     } catch (ExceptionCredencialesInvalidas e) {
                         System.out.println(e.getMessage());
                     }
-
                 }
                 case 2 -> {
-
-
-
                     System.out.println("Ingrese nombre: ");
                     String nombre = teclado.nextLine();
 
                     int dni = ConsolaUtils.leerEntero(teclado, "Ingrese dni:");
+                    teclado.nextLine();
 
                     System.out.println("Ingrese origen: ");
                     String origen = teclado.nextLine();
@@ -130,31 +117,26 @@ public class Main {
                     System.out.println("Ingrese contraseña: ");
                     String contra = teclado.nextLine();
 
-
                     try {
 
                          String msg = miHotel.RegistrarCliente(nombre, dni, origen, direccion, email, contra);
-                        sistemaUsuarios.guardarEnJSON();
                          System.out.println(msg);
+
                     } catch (ExceptionUsuarioDuplicado e) {
-                        System.out.println(e.getMessage());
+                         System.out.println(e.getMessage());
+
                     }
 
-
                 }default -> {
-                    System.out.println("ERROR,ingrese 1,2,3 o 0");
+                    System.out.println("Opcion invalida, intente nuevamente...");
                 }
             }
-
-
-
-
-
         }while(opcion!=0);
 
+        System.out.println("Guardando datos del hotel...");
         miHotel.guardarEnJSON();
         miHotel.getSistemaUsuarios().guardarEnJSON();
-        System.out.println("Guardando datos del hotel...");
+        System.out.println("Datos guardados correctamente..");
     }
 
 }
