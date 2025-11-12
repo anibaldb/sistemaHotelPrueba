32d37623f958cceb8a29ad0098e231310f0317d7
anibaldb <128552893+anibaldb@users.noreply.github.com>
2025-11-11 21:10:38 -0300

    sistema usuraios ordenado

--- src/Clases/SistemaUsuarios.java
+++ src/Clases/SistemaUsuarios.java
@@ -19,9 +19,6 @@ public class SistemaUsuarios {
         this.usuarioLogueado = null;
     }
 
-    public void agregarUsuario(Usuario usuario) {
-        this.usuarios.add(usuario);
-    }
 
     public Usuario getUsuarioLogueado() {
         return usuarioLogueado;
@@ -38,13 +35,10 @@ public class SistemaUsuarios {
                 return u;
             }
         }
-
         throw new ExceptionCredencialesInvalidas("Email o contraseña invalida");
-
     }
 
     public String  registrarAdministrador(String nombre, int dni, String origen, String direccionOrigen, String eMail, String contrasenia) throws ExceptionUsuarioDuplicado {
-
 
         if (verificarDuplicado(dni)) {
             throw new ExceptionUsuarioDuplicado("El usuario con ese DNI ya existe...");
@@ -53,12 +47,10 @@ public class SistemaUsuarios {
         usuarios.add(usuario);
         guardarEnJSON();
 
-        return "Admin registrado con exito...";
+        return "Administrador registrado con éxito...";
     }
 
-
     public String  registrarCliente(String nombre, int dni, String origen, String direccionOrigen, String eMail, String contrasenia) throws ExceptionUsuarioDuplicado {
-
 
         if (verificarDuplicado(dni)) {
             throw new ExceptionUsuarioDuplicado("El usuario con ese DNI ya existe...");
@@ -73,7 +65,6 @@ public class SistemaUsuarios {
 
     public String  registrarRecepcionista(String nombre, int dni, String origen, String direccionOrigen, String eMail, String contrasenia) throws ExceptionUsuarioDuplicado {
 
-
         if (verificarDuplicado(dni)) {
             throw new ExceptionUsuarioDuplicado("El usuario con ese DNI ya existe...");
         }
@@ -81,20 +72,16 @@ public class SistemaUsuarios {
         usuarios.add(usuario);
         guardarEnJSON();
 
-
         return "Recepcionista registrado con éxito...";
     }
-
 
     public boolean verificarDuplicado(int dni) {
         for (Usuario u : usuarios) {
             if (u.getDni() == dni) {
                 return true;
             }
-
         }
         return false;
-
     }
 
     public void listarUsuarios(){
@@ -111,6 +98,10 @@ public class SistemaUsuarios {
         return null;
     }
 
+    public void agregarUsuario(Usuario usuario) {
+        this.usuarios.add(usuario);
+    }
+
     public JSONObject toJSON() {
         JSONObject json = new JSONObject();
         JSONArray usuariosArray = new JSONArray();
@@ -122,7 +113,6 @@ public class SistemaUsuarios {
         json.put("usuarios", usuariosArray);
         return json;
     }
-
 
     public static SistemaUsuarios fromJSON(JSONObject json) {
         SistemaUsuarios sistema = new SistemaUsuarios();
@@ -141,8 +131,6 @@ public class SistemaUsuarios {
         JSONObject jsonSistema = this.toJSON();
         JSONUtiles.uploadJSON(jsonSistema, "usuarios");
     }
-
-
 }
 
 
